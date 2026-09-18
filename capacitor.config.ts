1d77b37e58c13cc6c4368157f27dd797ec8357f7
Mulima A Philip(P White) <102904206+pamtechz@users.noreply.github.com>
2026-09-18 22:12:38 +0200

    Set VOP Android application ID to com.sda.vop

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.rvc.vopapp',
-  appName: 'VOP App',
-  webDir: 'dist'
+  appId: 'com.sda.vop',
+  appName: 'Voice of Prophecy',
+  webDir: 'dist',
 };
 
 export default config;
